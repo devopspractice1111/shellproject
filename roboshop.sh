98f108e33ca394f27d26c3177f111da3d340c356
SreekanthMadhava75 <sreekanth.madhava75@gmail.com>
2024-07-31 19:29:07 +0530

    shellscript project

--- roboshop.sh
+++ roboshop.sh
@@ -8,14 +8,14 @@ DOMAIN_NAME=devopsbysreekanth.oneline
 
 for i in "${INSTANCES[@]}"
 do
-    if[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
-        INSTANCE_TYPE= t3.small
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE= t2.micro
+        INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS= $(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-01f3da9a50049f833 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
